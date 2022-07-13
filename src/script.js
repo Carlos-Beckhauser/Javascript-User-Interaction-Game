0d5a9cbbdeb69410c85dfc3aa656f55ebda0bd0c
Carlos <Cbeckhauser@gmail.com>
2022-07-13 10:13:45 -0300

    instructions button added

--- src/script.js
+++ src/script.js
@@ -1,19 +1,65 @@
+/* document.addEventListener("DOMContentLoaded", () => {
+  const handleClick = () => {
+    const body = document.querySelector("body");
+
+    body.innerHTML = `
+          <h1>Hello Javascript!!!<h1>
+      `;
+
+    body.style.backgroundColor = "#000";
+    body.style.color = "#fff";
+  };
+
+  const el = document.getElementById("btn-start");
+
+  el.addEventListener("mouseover", () => {
+    el.style.position = "absolute";
+    el.style.top = `${Math.floor(Math.random() * (window.innerHeight - 30))}px`;
+    el.style.left = `${Math.floor(Math.random() * (window.innerWidth - 84))}px`;
+  });
+  el.addEventListener("click", () => handleClick());
+});
+ */
+
 const handleClick = () => {
-  const body = document.querySelector("body");
+  const body = $("body");
 
-  body.innerHTML = `
-        <h1>Hello Javascript!!!<h1>
-    `;
+  body.html(`
+          <h1>Hello Javascript!!!<h1>
+          <h2>and hello world!!!<h2>
+      `);
 
-  body.style.backgroundColor = "#000";
-  body.style.color = "#fff";
+  body.css("backgroundColor", "#000");
+  body.css("color", "#fff");
 };
 
-const el = document.getElementById("btn-start");
+const startButtonInit = () => {
+  const el = $("#btn-start");
 
-el.addEventListener("mouseover", () => {
-  el.style.position = "absolute";
-  el.style.top = `${Math.floor(Math.random() * (window.innerHeight - 30))}px`;
-  el.style.left = `${Math.floor(Math.random() * (window.innerWidth - 84))}px`;
+  el.on("mouseover", () => {
+    el.css("position", "absolute");
+    el.css("top", `${Math.floor(Math.random() * (window.innerHeight - 30))}px`);
+    el.css("left", `${Math.floor(Math.random() * (window.innerWidth - 84))}px`);
+  });
+  el.on("click", () => handleClick());
+};
+
+const infoButtonInit = () => {
+  const el = $("#btn-info");
+  const elInitialText = el.html();
+  el.on("click", () => {
+    if (el.html() == elInitialText) {
+      $(".info").slideDown();
+
+      el.html("OCULTAR INFORMAÇÕES");
+    } else {
+      el.html(elInitialText);
+      $(".info").slideUp();
+    }
+  });
+};
+
+$(document).ready(() => {
+  startButtonInit();
+  infoButtonInit();
 });
-el.addEventListener("click", () => handleClick());
